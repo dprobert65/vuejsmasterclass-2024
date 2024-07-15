9f813f2e5c6614ffeb792fb7924991f16d094a44
dave probert <dprobert.gmail.com>
2024-07-15 15:00:07 -0500

    chapter3-3

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+console.log(supabase)
+</script>
+
+// do not delete: RahthdmRKeAB425h
 
 <template>
   <main>
